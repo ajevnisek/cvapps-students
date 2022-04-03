43a5c83cac9c96393d20d9d8703f0be81c8a0980
Amir Jevnisek <ajevnisek@gmail.com>
2022-04-03 14:17:28 +0300

    HW3 environment.yml

--- src/tests/test_packages_versions.py
+++ src/tests/test_packages_versions.py
@@ -3,11 +3,7 @@ import pytest
 
 
 def test_cv2_version():
-    assert (cv2.__version__ == '4.5.2')
-
-
-def test_scipy_version():
-    assert (scipy.__version__ == '1.8.0')
+    assert (cv2.__version__ == '4.5.4')
 
 
 def test_matplotlib_version():
@@ -15,4 +11,4 @@ def test_matplotlib_version():
 
 
 def test_numpy_version():
-    assert (np.__version__ == '1.21.1')
+    assert (np.__version__ == '1.19.5')
