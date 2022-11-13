b6c51a2933f2fbc2dcc08669c666d8ad1cc95dfd
Amir Jevnisek <ajenvisek@gmail.com>
2022-11-13 10:16:59 +0200

    Exercise 4 environment yml.

--- src/tests/test_packages_versions.py
+++ src/tests/test_packages_versions.py
@@ -2,17 +2,14 @@ from ..cvapps_sol1_checker import *
 import pytest
 
 
-def test_cv2_version():
-    assert (cv2.__version__ == '4.5.4')
-
 
 def test_matplotlib_version():
     assert (matplotlib.__version__ == '3.4.3')
 
 
 def test_numpy_version():
-    assert (np.__version__ == '1.22.3')
+    assert (np.__version__ == '1.23.4')
 
 def test_tqdm_version():
-    assert (tqdm.__version__ == '4.64.0')
+    assert (tqdm.__version__ == '4.64.1')
 
