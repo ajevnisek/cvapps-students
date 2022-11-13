99f2c52c5ddb041c412b367f84a06527914dd4f2
Amir Jevnisek <ajenvisek@gmail.com>
2022-11-13 10:41:56 +0200

    Add opencv support and test.

--- src/tests/test_packages_versions.py
+++ src/tests/test_packages_versions.py
@@ -2,6 +2,8 @@ from ..cvapps_sol1_checker import *
 import pytest
 
 
+def test_cv2_version():
+    assert(cv2.__version__ == '4.6.0')
 
 def test_matplotlib_version():
     assert (matplotlib.__version__ == '3.4.3')
