dd88bc1a8b83d401c709e06ceb2e01c3a274e0d6
Amir Jevnisek <ajenvisek@gmail.com>
2023-03-24 13:35:14 +0300

    Validate libraries versions.

--- src/tests/test_packages_versions.py
+++ src/tests/test_packages_versions.py
@@ -3,15 +3,15 @@ import pytest
 
 
 def test_cv2_version():
-    assert(cv2.__version__ == '4.6.0')
+    assert(cv2.__version__ == '4.5.2')
 
 def test_matplotlib_version():
-    assert (matplotlib.__version__ == '3.4.3')
+    assert (matplotlib.__version__ == '3.5.1')
 
 
 def test_numpy_version():
-    assert (np.__version__ == '1.23.4')
+    assert (np.__version__ == '1.21.1')
 
 def test_tqdm_version():
-    assert (tqdm.__version__ == '4.64.1')
+    assert (tqdm.__version__ == '4.63.0')
 
